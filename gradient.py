d31896ec46cfd50dc7a1e1a737380c884bbd1da9
atg-207 <tungdz207@gmail.com>
2026-05-06 21:32:56 +0700

    refine + new result

--- gradient.py
+++ gradient.py
@@ -4,16 +4,25 @@ def f(x):
 def df(x):
     return 2*x
 
-def gradient_descent(x_start, learning_rate, epochs):
+def gradient_descent(x_start, learning_rate, threshold):
     x = x_start
-    print(f"{'Step':<10} | {'x':<10} | {'f(x)':<10}")
+    time = 0
+
+    print(f"{'Time':<10} | {'x':<10} | {'f(x)':<10}")
     print("-" * 35)
-    for i in range(epochs):
+
+    while f(x) > threshold:
+        time += 1
+
         grad = df(x)
         x = x - learning_rate * grad
-        print(f"{i+1:<10} | {x:<10.4f} | {f(x):<10.4f}")  
+
+        print(f"{time:<10} | {x:<10.4f} | {f(x):<10.4f}")
+
+        if time >= 100:
+            break
+
     return x
 
 if __name__ == "__main__":
-    final_x = gradient_descent(x_start=10, learning_rate=0.1, epochs=10)
-    print(f"The local minimum occurs at {final_x:.4f}")
+    gradient_descent(x_start=10, learning_rate=0.1, threshold=0.01)
